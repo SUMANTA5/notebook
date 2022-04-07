60f00065978e6d04c8a02e01ecfb253e8f96ca3d
SUMANTA5 <sumanta111195@gmail.com>
2022-04-07 15:34:28 +0530

    fix

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -4,10 +4,10 @@ const Noteitem = (props) => {
   const { note } = props;
   return (
     <div className="col-md-3">
-      <div class="card my-3">
-        <div class="card-body">
-          <h5 class="card-title">{note.title}</h5>
-          <p class="card-text">{note.description}</p>
+      <div className="card my-3">
+        <div className="card-body">
+          <h5 className="card-title">{note.title}</h5>
+          <p className="card-text">{note.description}</p>
         </div>
       </div>
     </div>
